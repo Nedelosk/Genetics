b491f860e168e7b49f3eba1bf1270863d3b3b0b8
Nedelosk <nedelosk@gmail.com>
2017-12-24 17:43:59 +0100

    Remove Api

--- src/main/java/nedelosk/crispr/api/package-info.java
+++ src/main/java/nedelosk/crispr/api/package-info.java
@@ -1,10 +1,7 @@
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
-@API(apiVersion = "1.0.0", owner = "crispr", provides = "crispr|api")
 package nedelosk.crispr.api;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-
-import net.minecraftforge.fml.common.API;
 
 import mcp.MethodsReturnNonnullByDefault;
