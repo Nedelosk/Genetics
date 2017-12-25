d8e09b92dde79d4c810da8c9b40bb943abc6b6f5
Nedelosk <nedelosk@gmail.com>
2017-12-25 02:04:30 +0100

    Improve Plugins

--- src/main/java/nedelosk/crispr/plugins/PluginManager.java
+++ src/main/java/nedelosk/crispr/plugins/PluginManager.java
@@ -7,8 +7,8 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import nedelosk.crispr.api.CrisprAPI;
 import nedelosk.crispr.api.IGeneticPlugin;
-import nedelosk.crispr.api.gene.IGeneRegistry;
 import nedelosk.crispr.apiimp.GeneticFactory;
+import nedelosk.crispr.apiimp.GeneticRegistry;
 import nedelosk.crispr.apiimp.gene.GeneRegistry;
 
 public class PluginManager {
@@ -22,10 +22,15 @@ public class PluginManager {
 	}
 
 	public static void createRegistry() {
-		IGeneRegistry geneRegistry = new GeneRegistry();
+		GeneRegistry geneRegistry = new GeneRegistry();
 		CrisprAPI.geneRegistry = geneRegistry;
 		for (IGeneticPlugin plugin : plugins) {
 			plugin.registerGenes(geneRegistry, GeneticFactory.INSTANCE);
+		}
+		GeneticRegistry geneticRegistry = geneRegistry.createGeneticRegistry();
+		CrisprAPI.registry = geneticRegistry;
+		for (IGeneticPlugin plugin : plugins) {
+			plugin.register(geneticRegistry);
 		}
 	}
 }
